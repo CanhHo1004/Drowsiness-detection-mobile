c06005146bdcb119925ea80862dee7173c1d2b9d
Ho Minh Canh <48144663+CanhHo1004@users.noreply.github.com>
2021-04-09 12:21:21 +0700

    12:15 9/4/21

--- led.py
+++ led.py
@@ -7,6 +7,7 @@ GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 try:
     GPIO.output(8, GPIO.HIGH)
     print('output is high')
+    sleep(1)
     GPIO.output(8, GPIO.LOW)
     print('output is low')
 except:
